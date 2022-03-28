06f71a2b268a931462dd5766b1c81a0a09e96c6c
unknown <mahmodulshahin665@gmail.com>
2022-03-28 19:31:29 +0600

    add balance total

--- js/functional.js
+++ js/functional.js
@@ -13,9 +13,27 @@ function updateTotal(fieldId, amount) {
     totalTag.innerText = newTotal;
 
 }
+function updateBalance(amount, isAddindg) {
+    const balanceTag = document.getElementById('balance-total');
+    const balanceInText = balanceTag.innerText;
+    const previousBalance = parseFloat(balanceInText);
+    let newBalance;
+    if (isAddindg == true) {
+        newBalance = previousBalance + amount;
+    }
+    else {
+        newBalance = previousBalance - amount;
+    }
+    balanceTag.innerText = newBalance;
+
+}
 document.getElementById('deposit-button').addEventListener('click', function () {
     const amount = getInputValue('deposit-input');
     updateTotal('deposit-total', amount);
-    console.log(amount);
-
+    updateBalance(amount, true)
 });
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const amount = getInputValue('withdraw-input');
+    updateTotal('withdraw-total', amount);
+    updateBalance(amount, false);
+})
